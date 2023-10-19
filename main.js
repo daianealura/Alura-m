fe827f7199d9f650e5235ef79c0fe05b4e12e6a5
Daiane <117860490+daianealura@users.noreply.github.com>
2023-10-19 10:59:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-document.querySelector('.tecla_pom') 
-document.querySelector('.tecla_clap') 
-document.querySelector('.tecla_tim') 
-document.querySelector('.tecla_puff') 
-document.querySelector('.tecla_splash') 
-document.querySelector('.tecla_toim') 
-document.querySelector('.tecla_psh') 
-document.querySelector('.tecla_tic') 
-document.querySelector('.tecla_tom') 
+alert ('Essa página contém vírus');
 
-document.querySelector('#som_tecla_pom').play
+function tocaSomPom () { document.querySelector('#som_tecla_pom').play ();}
+
+
+document.querySelector('.tecla_pom'). onclick =tocaSomPom;
+
+document.querySelector('.tecla_clap') ;
+document.querySelector('.tecla_tim') ;
+document.querySelector('.tecla_puff');
+document.querySelector('.tecla_splash') ;
+document.querySelector('.tecla_toim') ;
+document.querySelector('.tecla_psh') ;;
+document.querySelector('.tecla_tic') ;
+document.querySelector('.tecla_tom') ;
+
